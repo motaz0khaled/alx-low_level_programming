ba5cdc5d82025b6ccdccb09c5612b86f63c81356
motaz0khaled <0motazkhaled0@gmail.com>
2023-06-20 12:08:43 -0700

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,12 @@
 	 * main - print _putchar
 	 * is written by mbah
 	 * Return: 0
-	 */
-
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 	int main(void)
 	{
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef main_h
 	#define main_h
 
+/*
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
 
 	int _putchar(char);
 	void print_alphabet(void);
