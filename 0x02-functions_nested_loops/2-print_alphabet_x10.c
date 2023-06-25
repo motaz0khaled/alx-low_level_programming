0fc84f3a34e6902ac9838dba54f084b0fdce0a3c
motaz khaled <97042005+motaz0khaled@users.noreply.github.com>
2023-06-26 00:30:10 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,16 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: motaz khaled
+ */
+
+
+
 #include "main.h"
 
 
 	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-	 * followed by a new line
-	 */
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
 	void print_alphabet_x10(void)
 	{
 		char ch;
